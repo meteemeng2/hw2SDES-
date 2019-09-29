b68f2334bdbf64e17caa0c8fc8b56c674e027a60
meteemeng2 <30922201+meteemeng2@users.noreply.github.com>
2019-09-29 20:52:39 +0700

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 20:35:50 2019
+
+@author: Admin
+"""
+import importlib
+
+moduleName = 'sdes'##from internet
+sdes = importlib.import_module(moduleName)
+
+
+mycode = [5,9,0,6,1,0,6,4,9]
+#mycodetest = ["00000101","00001001","00000000","00000110","00000001","00000000","00000110","00000100","00001001"]
+webcipher = "0b11010111,0b10111100,0b10100010,0b1011101,0b10010010,0b10100010,0b1011101,0b111,0b10111100,0b111,0b111,0b1101001,0b1011101,0b1101001,0b111,0b111,0b111111,0b11010111,0b111,0b10111110,0b10111100,0b11000,0b1011101,0b11010111,0b10010010,0b1101001,0b111111,0b10111100,0b10010010,0b10100010,0b111111,0b10111100,0b10111110,0b1101001,0b10111110,0b10100010,0b10010010,0b1101001,0b10100010,0b10010010,0b11010111,0b11000,0b11010111,0b11010111,0b11000,0b11000,0b10111100,0b1011101,0b10010010,0b10010010,0b111,0b111,0b1011101,0b10100010,0b10010010,0b11010111,0b10111110,0b111111,0b1011101,0b111,0b11010111,0b10111100,0b10010010,0b10010010,0b10111100,0b11000,0b111111,0b111,0b111111,0b111111,0b1101001,0b10010010,0b10100010,0b1101001,0b10010010,0b1101001,"
+
+
+
+mykeylist = []
+for i in range (1024):
+    mykeylist.append(bin(i)[2:].zfill(10))
+
+newkeylist = []
+        
+student_encode = webcipher[:-1].split(",")[0:9]
+
+print("==== test each 590610649 -[key]-> cipher-text ====")
+for i in range(len(student_encode)): #590610649
+    eachcipher = student_encode[i][2:].zfill(8)
+    mycipher = bin(mycode[i])[2:].zfill(8)
+
+    newkeylist = []
+    
+    for j in range (len(mykeylist)): # 2^10  key
+        
+        mybruteforce = sdes.enc(mykeylist[j],mycipher)
+        
+        if(mybruteforce == eachcipher):
+            newkeylist.append(int(mykeylist[j],2))
+            
+    print("code",mycode[i],"to binary ",mycipher," keys found = ",newkeylist)
+print("(key in decimal)")
+
+print()
+print("==== test each cipher-text -[key]-> 590610649 ====")
+for i in range(len(student_encode)): #590610649
+    eachcipher = student_encode[i][2:].zfill(8)
+    mycipher = bin(mycode[i])[2:].zfill(8)
+
+    newkeylist = []
+    
+    for j in range (len(mykeylist)): # 2^10  key
+        
+        mybruteforce = sdes.dec(mykeylist[j],eachcipher)
+        
+        if(mybruteforce == mycipher):
+            newkeylist.append(int(mykeylist[j],2))
+            
+    print("cipher ",eachcipher," keys found = ",newkeylist)
+print("(key in decimal)")
+#print(mykeylist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ sdes.py
@@ -0,0 +1,111 @@
+__author__ = 'ßaron'
+
+FIXED_IP = [2, 6, 3, 1, 4, 8, 5, 7]
+FIXED_EP = [4, 1, 2, 3, 2, 3, 4, 1]
+FIXED_IP_INVERSE = [4, 1, 3, 5, 7, 2, 8, 6]
+FIXED_P10 = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
+FIXED_P8 = [6, 3, 7, 4, 8, 5, 10, 9]
+FIXED_P4 = [2, 4, 3, 1]
+
+S0 = [[1, 0, 3, 2],
+      [3, 2, 1, 0],
+      [0, 2, 1, 3],
+      [3, 1, 3, 2]]
+
+S1 = [[0, 1, 2, 3],
+      [2, 0, 1, 3],
+      [3, 0, 1, 0],
+      [2, 1, 0, 3]]
+
+KEY = 'testerror'
+
+
+def permutate(original, fixed_key):
+    new = ''
+    for i in fixed_key:
+        new += original[i - 1]
+    return new
+
+
+def left_half(bits):
+    return bits[:int(len(bits)/2)]
+
+
+def right_half(bits):
+    return bits[int(len(bits)/2):]
+
+
+def shift(bits):
+    rotated_left_half = left_half(bits)[1:] + left_half(bits)[0]
+    rotated_right_half = right_half(bits)[1:] + right_half(bits)[0]
+    return rotated_left_half + rotated_right_half
+
+
+def key1():
+    return permutate(shift(permutate(KEY, FIXED_P10)), FIXED_P8)
+
+
+def key2():
+    return permutate(shift(shift(shift(permutate(KEY, FIXED_P10)))), FIXED_P8)
+
+
+def xor(bits, key):
+    new = ''
+    for bit, key_bit in zip(bits, key):
+        new += str(((int(bit) + int(key_bit)) % 2))
+    return new
+
+
+def lookup_in_sbox(bits, sbox):
+    row = int(bits[0] + bits[3], 2)
+    col = int(bits[1] + bits[2], 2)
+    return '{0:02b}'.format(sbox[row][col])
+
+
+def f_k(bits, key):
+    L = left_half(bits)
+    R = right_half(bits)
+    bits = permutate(R, FIXED_EP)
+    bits = xor(bits, key)
+    bits = lookup_in_sbox(left_half(bits), S0) + lookup_in_sbox(right_half(bits), S1)
+    bits = permutate(bits, FIXED_P4)
+    return xor(bits, L)
+
+
+def encrypt(plain_text):
+    bits = permutate(plain_text, FIXED_IP)
+    temp = f_k(bits, key1())
+    bits = right_half(bits) + temp
+    bits = f_k(bits, key2())
+    return (permutate(bits + temp, FIXED_IP_INVERSE))
+
+
+def decrypt(cipher_text):
+    bits = permutate(cipher_text, FIXED_IP)
+    temp = f_k(bits, key2())
+    bits = right_half(bits) + temp
+    bits = f_k(bits, key1())
+    return (permutate(bits + temp, FIXED_IP_INVERSE))
+
+def enc(inputkey,cipher):
+    global KEY
+    KEY = inputkey
+    
+    return encrypt(cipher)
+    #encrypt('00000101')
+    #decrypt('10100010')
+
+def dec(inputkey,cipher):
+    global KEY
+    KEY = inputkey
+    
+    return decrypt(cipher)
+    
+    
+    
+    
+    
+    
+    
+    
+    
